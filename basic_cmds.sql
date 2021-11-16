eadcedbb0cca8b8c68067debcf2d1721f83802b5
duartele <lndoxmat@hotmail.com>
2021-11-16 20:21:51 +0000

    Create basic_cmds.sql

---
+++ basic_cmds.sql
@@ -0,0 +1,15 @@
+#See if a database is active
+#status - just work in the terminal/console
+#exit - just work in the terminal/console
+
+# Active a database
+use college;
+
+#Some useful cmds
+describe scholarship;
+desc students; #here desc means describe 
+show databases;
+show tables;
+drop database xyz;
+
+
